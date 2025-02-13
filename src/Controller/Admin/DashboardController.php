3be6c4e1d287b56fc297c654aae829e4ef823ebd
samenthalo <ortega.samantha81400@gmail.com>
2025-02-13 16:18:15 +0100

    ajout securité admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,38 +2,40 @@
 
 namespace App\Controller\Admin;
 
-
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use App\Controller\Admin\PostCrudController;
 use App\Controller\Admin\CommentCrudController;
-use App\Entity\Comment;
 use App\Entity\Post;
+use App\Entity\Comment;
 
 class DashboardController extends AbstractDashboardController
 {
-    #[Route('/admin', name: 'admin',)]
+    private $authChecker;
+
+    public function __construct(AuthorizationCheckerInterface $authChecker)
+    {
+        $this->authChecker = $authChecker;
+    }
+
+    #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
+        //vérifie si l'utilisateur a le rôle 'ROLE_ADMIN'
+        if (!$this->authChecker->isGranted('ROLE_ADMIN')) {
+            //ajoute un message flash indiquant que l'utilisateur n'a pas accès
+            $this->addFlash('error', 'Vous n\'êtes pas administrateur, vous n\'avez pas le droit d\'accéder à cette page.');
+
+            //redirige vers la page d'accueil
+            return $this->redirectToRoute('home');
+        }
+
+        //si l'utilisateur est un administrateur, affiche le tableau de bord
         return $this->render('/admin/dashboard.html.twig');
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -45,8 +47,9 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Tableau de bord', 'fa fa-home');
-        yield MenuItem::LinktoCrud('Article', 'fa fa-posts', Post::class);
+        yield MenuItem::linkToCrud('Article', 'fa fa-posts', Post::class);
         yield MenuItem::linkToCrud('Commentaires', 'fa fa-comments', Comment::class);
-
     }
 }
+
+
